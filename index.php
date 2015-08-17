cab1172a95027019350653c06326c18342a80854
julianamayercardoso <julianamayercardoso@gmail.com>
2015-08-17 20:55:02 -0300

    testes de php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Testes de html 5, ecmascript 6, css 3 e php 5</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	</head>
+	<body>
+		<?php
+		echo "Juliana e alexsandro Stefenon<br>";
+		echo "1,2,3,4,5";
+		?>
+	</body>
+</html>
